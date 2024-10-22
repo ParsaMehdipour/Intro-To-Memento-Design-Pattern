2d284cb564d621f2975b5d717aeb515221a1cdc5
Parsa <mehdipourparsa@gmail.com>
2024-10-22 19:50:12 -0400

    feat: Program.cs

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿// Client code
+using DemoLibrary.Caretakes;
+using DemoLibrary.Originators;
+
+DocumentEditor editor = new DocumentEditor("Initial document text.");
+DocumentCaretaker caretaker = new DocumentCaretaker(editor);
+
+caretaker.Backup();
+editor.EditText();
+
+caretaker.Backup();
+editor.ChangeFont();
+
+caretaker.Backup();
+editor.EditText();
+
+Console.WriteLine();
+caretaker.ShowHistory();
+
+Console.WriteLine("\nClient: Let's rollback!\n");
+caretaker.Undo();
+
+Console.WriteLine("\nClient: Once more!\n");
+caretaker.Undo();
