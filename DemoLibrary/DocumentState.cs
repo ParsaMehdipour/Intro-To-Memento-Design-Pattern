e01406b68d04c63eb4331d84f86d5692d8b019eb
Parsa <mehdipourparsa@gmail.com>
2024-10-22 19:50:00 -0400

    feat: DocumentState

---
+++ DemoLibrary/DocumentState.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoLibrary;
+
+/// <summary>
+/// Document State class to hold the state
+/// </summary>
+public class DocumentState
+{
+    public string Text { get; private set; }
+    public string FontName { get; private set; }
+    public int FontSize { get; private set; }
+    public ConsoleColor TextColor { get; private set; }
+
+    public DocumentState(string text, string fontName, int fontSize, ConsoleColor textColor)
+    {
+        Text = text;
+        FontName = fontName;
+        FontSize = fontSize;
+        TextColor = textColor;
+    }
+}
